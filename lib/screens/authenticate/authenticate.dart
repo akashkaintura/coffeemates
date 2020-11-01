062119690a11e191c86ac6aa1583f6a1704997d6
Cole.Akash <cole.akash@Akash.local>
2020-11-01 09:01:14 +0530

    first commit

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _Authenticate createState() => _Authenticate();
+}
+
+class _Authenticate extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('authenticate'),
+    );
+  }
+}
